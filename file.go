05d351993dff5660a7e5693577770021fb8ddb26
kakuilan <kakuilan@163.com>
2019-06-28 11:52:33 +0800

    LkkFile add IsFile/IsDir

--- file.go
+++ file.go
@@ -7,38 +7,64 @@ import (
 	"syscall"
 )
 
-// IsExist determines whether the path spcified by the given is exists.
-func(* LkkFile)  IsExist(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil || os.IsExist(err)
-}
-
 // GetExt get the file extension without a dot.
-func(* LkkFile) GetExt(path string) string {
+func(kf *LkkFile) GetExt(path string) string {
 	suffix := filepath.Ext(path)
 	if suffix != "" {
 		return strings.ToLower(suffix[1:])
 	}
-
 	return suffix
 }
 
 // GetSize get the length in bytes of file of the specified path.
-func(* LkkFile) GetSize(path string) int64 {
+func(kf *LkkFile) GetSize(path string) int64 {
 	f, err := os.Stat(path)
 	if nil != err {
 		return -1
 	}
-
 	return f.Size()
 }
 
+// IsExist determines whether the path spcified by the given is exists.
+func(kf *LkkFile)  IsExist(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
+}
+
 // Writeable determines whether the path spcified by the given path is writeable.
-func(* LkkFile) IsWritable(path string) bool {
+func(kf *LkkFile) IsWritable(path string) bool {
 	err := syscall.Access(path, syscall.O_RDWR)
 	if err != nil {
 		return false
 	}
-
 	return true
+}
+
+// IsReadable determines whether the path spcified by the given path is readable.
+func(kf *LkkFile) IsReadable(path string) bool {
+	if !kf.IsExist(path) {
+		return false
+	}
+	return syscall.Access(path, syscall.O_RDONLY) == nil
+}
+
+// IsFile returns true if path exists and is a file (or a link to a file) and false otherwise
+func(kf *LkkFile) IsFile(path string) bool {
+	stat, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return stat.Mode().IsRegular()
+}
+
+// IsDir determines whether the specified path is a directory.
+func (kf *LkkFile) IsDir(path string) bool {
+	f, err := os.Lstat(path)
+	if os.IsNotExist(err) {
+		return false
+	}else if nil != err {
+		return false
+	}
+
+	return f.IsDir()
 }
