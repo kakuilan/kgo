ba5461ae2f74335f2904d527adb4a0119a2b52b0
kakuilan <kakuilan@163.com>
2019-06-28 12:34:31 +0800

    add LkkFile IsBinary/IsImg

--- file.go
+++ file.go
@@ -8,7 +8,7 @@ import (
 )
 
 // GetExt get the file extension without a dot.
-func(kf *LkkFile) GetExt(path string) string {
+func (kf *LkkFile) GetExt(path string) string {
 	suffix := filepath.Ext(path)
 	if suffix != "" {
 		return strings.ToLower(suffix[1:])
@@ -17,7 +17,7 @@ func(kf *LkkFile) GetExt(path string) string {
 }
 
 // GetSize get the length in bytes of file of the specified path.
-func(kf *LkkFile) GetSize(path string) int64 {
+func (kf *LkkFile) GetSize(path string) int64 {
 	f, err := os.Stat(path)
 	if nil != err {
 		return -1
@@ -26,13 +26,13 @@ func(kf *LkkFile) GetSize(path string) int64 {
 }
 
 // IsExist determines whether the path spcified by the given is exists.
-func(kf *LkkFile)  IsExist(path string) bool {
+func (kf *LkkFile) IsExist(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil || os.IsExist(err)
 }
 
 // Writeable determines whether the path spcified by the given path is writeable.
-func(kf *LkkFile) IsWritable(path string) bool {
+func (kf *LkkFile) IsWritable(path string) bool {
 	err := syscall.Access(path, syscall.O_RDWR)
 	if err != nil {
 		return false
@@ -41,16 +41,16 @@ func(kf *LkkFile) IsWritable(path string) bool {
 }
 
 // IsReadable determines whether the path spcified by the given path is readable.
-func(kf *LkkFile) IsReadable(path string) bool {
+func (kf *LkkFile) IsReadable(path string) bool {
 	err := syscall.Access(path, syscall.O_RDONLY)
 	if err != nil {
-		return  false
+		return false
 	}
 	return true
 }
 
 // IsFile returns true if path exists and is a file (or a link to a file) and false otherwise
-func(kf *LkkFile) IsFile(path string) bool {
+func (kf *LkkFile) IsFile(path string) bool {
 	stat, err := os.Stat(path)
 	if err != nil {
 		return false
@@ -63,8 +63,29 @@ func (kf *LkkFile) IsDir(path string) bool {
 	f, err := os.Lstat(path)
 	if os.IsNotExist(err) {
 		return false
-	}else if nil != err {
+	} else if nil != err {
 		return false
 	}
 	return f.IsDir()
+}
+
+// IsBinary determines whether the specified content is a binary file content.
+func (kf *LkkFile) IsBinary(content string) bool {
+	for _, b := range content {
+		if 0 == b {
+			return true
+		}
+	}
+	return false
+}
+
+// IsImg determines whether the specified path is a image.
+func (kf *LkkFile) IsImg(path string) bool {
+	ext := kf.GetExt(path)
+	switch ext {
+	case "jpg", "jpeg", "bmp", "gif", "png", "svg", "ico":
+		return true
+	default:
+		return false
+	}
 }
