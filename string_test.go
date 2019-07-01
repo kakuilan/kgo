a357fcce62aefa00691f5d94bd761b81912b573e
kakuilan <kakuilan@163.com>
2019-07-01 17:23:19 +0800

    add TestStripTags

--- string_test.go
+++ string_test.go
@@ -2,6 +2,7 @@ package gohelper
 
 import (
 	"testing"
+	"strings"
 )
 
 func TestNl2br(t *testing.T) {
@@ -9,7 +10,10 @@ func TestNl2br(t *testing.T) {
 world!
 你好！`
 	res := KStr.Nl2br(str)
-	println(res)
+	if !strings.Contains(res, "<br />") {
+		t.Error("Nl2br fail")
+		return
+	}
 }
 
 func BenchmarkNl2br(b *testing.B) {
@@ -19,5 +23,28 @@ world!
 你好！`
 	for i:=0;i<b.N;i++{
 		_ = KStr.Nl2br(str)
+	}
+}
+
+func TestStripTags(t *testing.T) {
+	str := `
+<h1>Hello world!</h1>
+<script>alert('你好！')</scripty>
+`
+	res := KStr.StripTags(str)
+	if strings.Contains(res, "<script>") {
+		t.Error("StripTags fail")
+		return
+	}
+}
+
+func BenchmarkStripTags(b *testing.B) {
+	b.ResetTimer()
+	str := `
+<h1>Hello world!</h1>
+<script>alert('你好！')</scripty>
+`
+	for i:=0;i<b.N;i++{
+		_ = KStr.StripTags(str)
 	}
 }
