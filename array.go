76dbdc3dc2c5cda4c0fc4c8ef4cd069531d59adb
kakuilan <kakuilan@163.com>
2019-07-03 10:28:34 +0800

    add InArray

---
+++ array.go
@@ -0,0 +1,26 @@
+package gohelper
+
+import "reflect"
+
+//元素是否在数组(切片/字典)内
+func (ka *LkkArray) InArray(needle interface{}, haystack interface{}) bool {
+	val := reflect.ValueOf(haystack)
+	switch val.Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			if reflect.DeepEqual(needle, val.Index(i).Interface()) {
+				return true
+			}
+		}
+	case reflect.Map:
+		for _, k := range val.MapKeys() {
+			if reflect.DeepEqual(needle, val.MapIndex(k).Interface()) {
+				return true
+			}
+		}
+	default:
+		panic("haystack: haystack type muset be slice, array or map")
+	}
+
+	return false
+}
---
+++ array_test.go
@@ -0,0 +1,24 @@
+package gohelper
+
+import (
+	"testing"
+)
+
+func TestInArray(t *testing.T) {
+	arr := [5] int {1,2,3,4,5}
+	it := 2
+	if !KArr.InArray(it, arr) {
+		t.Error("InArray fail")
+		return
+	}
+}
+
+func BenchmarkInArray(b *testing.B) {
+	b.ResetTimer()
+	sli := []string {"a", "b", "c", "d", "e"}
+	it := "d"
+	for i:=0;i<b.N;i++{
+		KArr.InArray(it, sli)
+	}
+}
+
