9435e31caf027d14d3e2b4ea3234f1c74393c39e
kakuilan <kakuilan@163.com>
2019-07-03 19:38:56 +0800

    add Float2Str

--- convert.go
+++ convert.go
@@ -1,6 +1,9 @@
 package gohelper
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // 将整数转换为字符串
 func (kc *LkkConvert) Int2Str(val interface{}) string {
@@ -9,6 +12,20 @@ func (kc *LkkConvert) Int2Str(val interface{}) string {
 	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
 		return fmt.Sprintf("%d", val)
 	// Type is not integers, return empty string
+	default:
+		return ""
+	}
+}
+
+// 将浮点数转换为字符串,length为小数位数
+func (kc *LkkConvert) Float2Str(val interface{}, length int) string {
+	switch val.(type) {
+	// Floats
+	case float32:
+		return strconv.FormatFloat(float64(val.(float32)), 'f', length, 32)
+	case float64:
+		return strconv.FormatFloat(val.(float64), 'f', length, 64)
+	// Type is not floats, return empty string
 	default:
 		return ""
 	}
--- convert_test.go
+++ convert_test.go
@@ -21,3 +21,30 @@ func BenchmarkInt2Str(b *testing.B) {
 		KConv.Int2Str(tim)
 	}
 }
+
+func TestIntFloat2Str(t *testing.T) {
+	fl := float32(1234.567890)
+	f2 := float64(1234.567890)
+	res1 := KConv.Float2Str(fl, 4)
+	res2 := KConv.Float2Str(f2, 8)
+	if fmt.Sprint(reflect.TypeOf(res1)) != fmt.Sprint(reflect.TypeOf(res2)) {
+		t.Error("Int2Str fail")
+		return
+	}
+}
+
+func Benchmark32Float2Str(b *testing.B) {
+	b.ResetTimer()
+	fl := float32(1234.567890)
+	for i := 0; i < b.N; i++ {
+		KConv.Float2Str(fl, 4)
+	}
+}
+
+func Benchmark64Float2Str(b *testing.B) {
+	b.ResetTimer()
+	f2 := float64(1234.567890)
+	for i := 0; i < b.N; i++ {
+		KConv.Float2Str(f2, 8)
+	}
+}
