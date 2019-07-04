9cd6cdce60118d2167585d6e0f4ed2462ffcc6c8
kakuilan <kakuilan@163.com>
2019-07-04 18:57:04 +0800

    fix KArr.InArray 测试代码覆盖率

--- array_test.go
+++ array_test.go
@@ -1,16 +1,45 @@
 package gohelper
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestInArray(t *testing.T) {
+	//数组
 	arr := [5]int{1, 2, 3, 4, 5}
 	it := 2
 	if !KArr.InArray(it, arr) {
 		t.Error("InArray fail")
 		return
 	}
+
+	//字典
+	mp := map[string]string{
+		"a": "aa",
+		"b": "bb",
+	}
+	it2 := "a"
+	it3 := "bb"
+	if KArr.InArray(it2, mp) {
+		t.Error("InArray fail")
+		return
+	} else if !KArr.InArray(it3, mp) {
+		t.Error("InArray fail")
+		return
+	}
+
+	//haystack类型不对
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recover...:", r)
+		}
+	}()
+	if KArr.InArray(it2, "abc") {
+		t.Error("InArray fail")
+		return
+	}
+
 }
 
 func BenchmarkInArray(b *testing.B) {
