b1d0fbe9abf53f8a1d81a8926b8bd45162e3a7ef
kakuilan <kakuilan@163.com>
2019-07-11 10:54:20 +0800

    add Ktime::Strtotime/Date/Checkdate/Sleep/Usleep

--- time.go
+++ time.go
@@ -16,3 +16,54 @@ func (kt *LkkTime) MilliTime() int64 {
 func (kt *LkkTime) MicroTime() int64 {
 	return time.Now().UnixNano() / int64(time.Microsecond)
 }
+
+// Strtotime 字符串转时间戳
+// KTime.Strtotime("2019-07-11 10:11:23") == 1562839883
+func (kt *LkkTime) Strtotime(str string) (int64, error) {
+	layout := "2006-01-02 15:04:05"
+	t, err := time.Parse(layout, str)
+	if err != nil {
+		return 0, err
+	}
+	return t.Unix(), nil
+}
+
+// Date 格式化时间戳
+// KTime.Date(1562811851, "2006-01-02 15:04:05") == "2019-07-11 10:24:11"
+func (kt *LkkTime) Date(timestamp int64, format string) string {
+	return time.Unix(timestamp, 0).Format(format)
+}
+
+// Checkdate 检查是否正常的日期
+func (kt *LkkTime) Checkdate(month, day, year int) bool {
+	if month < 1 || month > 12 || day < 1 || day > 31 || year < 1 || year > 32767 {
+		return false
+	}
+	switch month {
+	case 4, 6, 9, 11:
+		if day > 30 {
+			return false
+		}
+	case 2:
+		// leap year
+		if year%4 == 0 && (year%100 != 0 || year%400 == 0) {
+			if day > 29 {
+				return false
+			}
+		} else if day > 28 {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Sleep 延缓执行,秒
+func (kt *LkkTime) Sleep(t int64) {
+	time.Sleep(time.Duration(t) * time.Second)
+}
+
+// Usleep 以指定的微秒数延迟执行
+func (kt *LkkTime) Usleep(t int64) {
+	time.Sleep(time.Duration(t) * time.Microsecond)
+}
--- time_test.go
+++ time_test.go
@@ -49,3 +49,78 @@ func BenchmarkMicroTime(b *testing.B) {
 		KTime.MicroTime()
 	}
 }
+
+func TestStrtotime(t *testing.T) {
+	ti, err := KTime.Strtotime("2019-07-11 10:11:23")
+	if err != nil || ti <= 0 {
+		t.Error("Strtotime fail")
+		return
+	}
+
+	_, err2 := KTime.Strtotime("02/01/2016 15:04:05")
+	if err2 == nil {
+		t.Error("Strtotime fail")
+		return
+	}
+}
+
+func BenchmarkStrtotime(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = KTime.Strtotime("2019-07-11 10:11:23")
+	}
+}
+
+func TestDate(t *testing.T) {
+	date1 := KTime.Date(1562811851, "2006-01-02 15:04:05")
+	date2 := KTime.Date(1562811851, "02/01/2006 15:04:05")
+	if date1 == "" || date2 == "" {
+		t.Error("Date fail")
+		return
+	}
+}
+
+func BenchmarkDate(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		KTime.Date(1562811851, "2006-01-02 15:04:05")
+	}
+}
+
+func TestCheckdate(t *testing.T) {
+	chk1 := KTime.Checkdate(7, 31, 2019)
+	chk2 := KTime.Checkdate(2, 31, 2019)
+	if !chk1 || chk2 {
+		t.Error("Checkdate fail")
+		return
+	}
+}
+
+func BenchmarkCheckdate(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		KTime.Checkdate(7, 31, 2019)
+	}
+}
+
+func TestSleep(t *testing.T) {
+	ti1 := KTime.Time()
+	KTime.Sleep(1)
+	ti2 := KTime.Time()
+	diff := ti2 - ti1
+	if diff != 1 {
+		t.Error("Sleep fail")
+		return
+	}
+}
+
+func TestUsleep(t *testing.T) {
+	ti1 := KTime.MicroTime()
+	KTime.Usleep(100)
+	ti2 := KTime.MicroTime()
+	diff := ti2 - ti1
+	if diff < 100 {
+		t.Error("Usleep fail")
+		return
+	}
+}
