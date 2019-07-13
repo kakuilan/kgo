e2ad6ac1f9b7fb1f80157a6c6983fa3c534fc226
kakuilan <kakuilan@163.com>
2019-07-13 10:36:04 +0800

    add KUrl.ParseStr

--- number_test.go
+++ number_test.go
@@ -7,9 +7,11 @@ import (
 func TestNumberFormat(t *testing.T) {
 	num1 := 123.4567890
 	num2 := -123.4567890
+	num3 := 123456789.1234567890
 	res1 := KNum.NumberFormat(num1, 3, ".", "")
 	res2 := KNum.NumberFormat(num2, 0, ".", "")
-	if res1 != "123.457" || res2 != "-123" {
+	res3 := KNum.NumberFormat(num3, 6, ".", ",")
+	if res1 != "123.457" || res2 != "-123" || res3 != "123,456,789.123457" {
 		t.Error("NumberFormat fail")
 		return
 	}
---
+++ url_test.go
@@ -0,0 +1,61 @@
+package kgo
+
+import (
+	"testing"
+)
+
+func TestParseStr(t *testing.T) {
+	str1 := `first=value&arr[]=foo+bar&arr[]=baz`
+	str2 := `f1=m&f2=n`
+	str3 := `f[a]=m&f[b]=n`
+	str4 := `f[a][a]=m&f[a][b]=n`
+	str5 := `f[]=m&f[]=n`
+	str6 := `f[a][]=m&f[a][]=n`
+	str7 := `f[][]=m&f[][]=n`
+	str8 := `f=m&f[a]=n`
+	str9 := `a .[[b=c`
+	arr1 := make(map[string]interface{})
+	arr2 := make(map[string]interface{})
+	arr3 := make(map[string]interface{})
+	arr4 := make(map[string]interface{})
+	arr5 := make(map[string]interface{})
+	arr6 := make(map[string]interface{})
+	arr7 := make(map[string]interface{})
+	arr8 := make(map[string]interface{})
+	arr9 := make(map[string]interface{})
+
+	err1 := KUrl.ParseStr(str1, arr1)
+	err2 := KUrl.ParseStr(str2, arr2)
+	err3 := KUrl.ParseStr(str3, arr3)
+	err4 := KUrl.ParseStr(str4, arr4)
+	err5 := KUrl.ParseStr(str5, arr5)
+	err6 := KUrl.ParseStr(str6, arr6)
+	err7 := KUrl.ParseStr(str7, arr7)
+	err8 := KUrl.ParseStr(str8, arr8)
+	err9 := KUrl.ParseStr(str9, arr9)
+	//fmt.Printf("%+v\n", arr1)
+	//fmt.Printf("%+v\n", arr2)
+	//fmt.Printf("%+v\n", arr3)
+	//fmt.Printf("%+v\n", arr4)
+	//fmt.Printf("%+v\n", arr5)
+	//fmt.Printf("%+v\n", arr6)
+	//fmt.Printf("%+v\n", arr7)
+	//fmt.Printf("%+v\n", arr9)
+
+	if err1 != nil || err2 != nil || err3 != nil || err4 != nil || err5 != nil || err6 != nil || err7 != nil || err9 != nil {
+		t.Error("ParseStr fail")
+		return
+	} else if err8 == nil {
+		t.Error("ParseStr fail")
+		return
+	}
+}
+
+func BenchmarkParseStr(b *testing.B) {
+	b.ResetTimer()
+	str := `first=value&arr[]=foo+bar&arr[]=baz`
+	arr := make(map[string]interface{})
+	for i := 0; i < b.N; i++ {
+		_ = KUrl.ParseStr(str, arr)
+	}
+}
