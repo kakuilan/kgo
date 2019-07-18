3747b9e9cba4fee3cd0cb4e320d3a80dc5436dc9
kakuilan <kakuilan@163.com>
2019-07-18 10:47:32 +0800

    add KStr.SimilarText

--- string.go
+++ string.go
@@ -323,7 +323,7 @@ func (ks *LkkString) Htmlentities(str string) string {
 	return html.EscapeString(str)
 }
 
-// HTMLEntityDecode 将HTML实体转换为它们对应的字符
+// HtmlentityDecode 将HTML实体转换为它们对应的字符
 func (ks *LkkString) HtmlentityDecode(str string) string {
 	return html.UnescapeString(str)
 }
@@ -331,4 +331,49 @@ func (ks *LkkString) HtmlentityDecode(str string) string {
 // Crc32 计算一个字符串的 crc32 多项式
 func (ks *LkkString) Crc32(str string) uint32 {
 	return crc32.ChecksumIEEE([]byte(str))
+}
+
+// SimilarText 计算两个字符串的相似度,返回在两个字符串中匹配字符的数目;percent为相似程度百分数
+func (ks *LkkString) SimilarText(first, second string, percent *float64) int {
+	var similarText func(string, string, int, int) int
+	similarText = func(str1, str2 string, len1, len2 int) int {
+		var sum, max int
+		pos1, pos2 := 0, 0
+
+		// Find the longest segment of the same section in two strings
+		for i := 0; i < len1; i++ {
+			for j := 0; j < len2; j++ {
+				for l := 0; (i+l < len1) && (j+l < len2) && (str1[i+l] == str2[j+l]); l++ {
+					if l+1 > max {
+						max = l + 1
+						pos1 = i
+						pos2 = j
+					}
+				}
+			}
+		}
+
+		if sum = max; sum > 0 {
+			if pos1 > 0 && pos2 > 0 {
+				sum += similarText(str1, str2, pos1, pos2)
+			}
+			if (pos1+max < len1) && (pos2+max < len2) {
+				s1 := []byte(str1)
+				s2 := []byte(str2)
+				sum += similarText(string(s1[pos1+max:]), string(s2[pos2+max:]), len1-pos1-max, len2-pos2-max)
+			}
+		}
+
+		return sum
+	}
+
+	l1, l2 := len(first), len(second)
+	if l1+l2 == 0 {
+		return 0
+	}
+	sim := similarText(first, second, l1, l2)
+	if percent != nil {
+		*percent = float64(sim*200) / float64(l1+l2)
+	}
+	return sim
 }
