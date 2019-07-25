3d2f30be6df2a3ce5e19058fb6163d227229282f
kakuilan <kakuilan@163.com>
2019-07-25 20:15:07 +0800

    add isArrayOrSlice

--- function.go
+++ function.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
+	"reflect"
 )
 
 // md5Str 计算字符串的 MD5 散列值
@@ -44,5 +45,23 @@ func sha256Str(str []byte) []byte {
 	hBytes := hash.Sum(nil)
 	res := make([]byte, hex.EncodedLen(len(hBytes)))
 	hex.Encode(res, hBytes)
+	return res
+}
+
+// isArrayOrSlice 检查变量是否数组或切片;chkType检查类型,枚举值有(1仅数组,2仅切片,3数组或切片);结果为-1表示非,>=0表示是
+func isArrayOrSlice(data interface{}, chkType uint8) int {
+	var res int = -1
+	val := reflect.ValueOf(data)
+	switch val.Kind() {
+	case reflect.Array:
+		if chkType == 1 || chkType == 3 {
+			res = val.Len()
+		}
+	case reflect.Slice:
+		if chkType == 2 || chkType == 3 {
+			res = val.Len()
+		}
+	}
+
 	return res
 }
