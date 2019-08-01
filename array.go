7df4b4de756fc65a27b45cc0606947874ec6c9b3
kakuilan <kakuilan@163.com>
2019-08-01 19:11:17 +0800

    add ArrayReverse

--- array.go
+++ array.go
@@ -423,3 +423,24 @@ func (ka *LkkArray) ArrayKeyExists(key interface{}, arr interface{}) bool {
 	}
 	return false
 }
+
+// ArrayReverse 返回单元顺序相反的数组(仅限数组和切片)
+func (ka *LkkArray) ArrayReverse(arr interface{}) []interface{} {
+	val := reflect.ValueOf(arr)
+	switch val.Kind() {
+	case reflect.Array, reflect.Slice:
+		length := val.Len()
+		res := make([]interface{}, length)
+		i, j := 0, length-1
+		for ; i < j; i, j = i+1, j-1 {
+			res[i], res[j] = val.Index(j).Interface(), val.Index(i).Interface()
+		}
+		if length > 0 && res[j] == nil {
+			res[j] = val.Index(j).Interface()
+		}
+
+		return res
+	default:
+		panic("[ArrayReverse]arr type must be array, slice")
+	}
+}
