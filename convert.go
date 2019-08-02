65c85f510af6da165328a577b7d0850bbfa5a687
kakuilan <kakuilan@163.com>
2019-08-02 11:19:22 +0800

    add Decbin/Bindec

--- convert.go
+++ convert.go
@@ -185,3 +185,17 @@ func (kc *LkkConvert) Str2ByteSlice(val string) []byte {
 func (kc *LkkConvert) BytesSlice2Str(val []byte) string {
 	return *(*string)(unsafe.Pointer(&val))
 }
+
+// Decbin 将十进制转换为二进制
+func (kc *LkkConvert) Decbin(number int64) string {
+	return strconv.FormatInt(number, 2)
+}
+
+// Bindec 将二进制转换为十进制
+func (kc *LkkConvert) Bindec(str string) (int64, error) {
+	i, err := strconv.ParseInt(str, 2, 0)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
