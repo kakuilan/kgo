76c7996b2f459cd5af7d2fddd078dffa412695ec
kakuilan <kakuilan@163.com>
2019-08-02 10:58:16 +0800

    add Max/Min

--- number_test.go
+++ number_test.go
@@ -70,6 +70,7 @@ func TestRand(t *testing.T) {
 		t.Error("Rand fail")
 		return
 	}
+	KNum.Rand(5, 5)
 }
 
 func TestRandPanicMin(t *testing.T) {
@@ -170,5 +171,57 @@ func BenchmarkPi(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		KNum.Pi()
+	}
+}
+
+func TestMax(t *testing.T) {
+	nums := []float64{-4, 0, 3, 9}
+	res := KNum.Max(nums...)
+	if int(res) != 9 {
+		t.Error("Max fail")
+		return
+	}
+}
+
+func TestMaxPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recover...:", r)
+		}
+	}()
+	KNum.Max(3)
+}
+
+func BenchmarkMax(b *testing.B) {
+	b.ResetTimer()
+	nums := []float64{-4, 0, 3, 9}
+	for i := 0; i < b.N; i++ {
+		KNum.Max(nums...)
+	}
+}
+
+func TestMin(t *testing.T) {
+	nums := []float64{-4, 0, 3, 9}
+	res := KNum.Min(nums...)
+	if int(res) != -4 {
+		t.Error("Min fail")
+		return
+	}
+}
+
+func TestMinPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recover...:", r)
+		}
+	}()
+	KNum.Min(3)
+}
+
+func BenchmarkMin(b *testing.B) {
+	b.ResetTimer()
+	nums := []float64{-4, 0, 3, 9}
+	for i := 0; i < b.N; i++ {
+		KNum.Min(nums...)
 	}
 }
