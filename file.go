2cb60cba38a0bcfba70a92e165cec3e500367b2b
kakuilan <kakuilan@163.com>
2019-08-03 10:00:06 +0800

    add Pathinfo

--- file.go
+++ file.go
@@ -545,3 +545,41 @@ func (kf *LkkFile) Sha256(path string) (string, error) {
 	hash.Write(data)
 	return hex.EncodeToString(hash.Sum(nil)), nil
 }
+
+// Pathinfo 获取文件路径的信息,options的值为-1: all; 1: dirname; 2: basename; 4: extension; 8: filename
+func (kf *LkkFile) Pathinfo(path string, options int) map[string]string {
+	if options == -1 {
+		options = 1 | 2 | 4 | 8
+	}
+	info := make(map[string]string)
+	if (options & 1) == 1 {
+		info["dirname"] = filepath.Dir(path)
+	}
+	if (options & 2) == 2 {
+		info["basename"] = filepath.Base(path)
+	}
+	if ((options & 4) == 4) || ((options & 8) == 8) {
+		basename := ""
+		if (options & 2) == 2 {
+			basename, _ = info["basename"]
+		} else {
+			basename = filepath.Base(path)
+		}
+		p := strings.LastIndex(basename, ".")
+		filename, extension := "", ""
+		if p > 0 {
+			filename, extension = basename[:p], basename[p+1:]
+		} else if p == -1 {
+			filename = basename
+		} else if p == 0 {
+			extension = basename[p+1:]
+		}
+		if (options & 4) == 4 {
+			info["extension"] = extension
+		}
+		if (options & 8) == 8 {
+			info["filename"] = filename
+		}
+	}
+	return info
+}
