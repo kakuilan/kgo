03d363be839fa8460376eb7bd5c237f08ce80806
kakuilan <kakuilan@163.com>
2019-09-23 13:49:43 +0800

    add CpuUsage

--- os.go
+++ os.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
@@ -13,6 +14,7 @@ import (
 	"os/user"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 	"unicode"
 )
@@ -195,6 +197,38 @@ func (ko *LkkOS) MemoryUsage() uint64 {
 	stat := new(runtime.MemStats)
 	runtime.ReadMemStats(stat)
 	return stat.Alloc
+}
+
+// CpuUsage 获取CPU使用率(仅支持linux),单位jiffies(节拍数).
+// user为用户态(用户进程)的运行时间,
+// idle为空闲时间,
+// total为累计时间.
+func (ko *LkkOS) CpuUsage() (user, idle, total uint64) {
+	contents, err := ioutil.ReadFile("/proc/stat")
+	if err != nil {
+		return
+	}
+	lines := strings.Split(string(contents), "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if fields[0] == "cpu" {
+			//CPU指标：user，nice, system, idle, iowait, irq, softirq
+			// cpu  130216 19944 162525 1491240 3784 24749 17773 0 0 0
+
+			numFields := len(fields)
+			for i := 1; i < numFields; i++ {
+				val, _ := strconv.ParseUint(fields[i], 10, 64)
+				total += val // tally up all the numbers to get total ticks
+				if i == 1 {
+					user = val
+				} else if i == 4 { // idle is the 5th field in the cpu line
+					idle = val
+				}
+			}
+			return
+		}
+	}
+	return
 }
 
 // Setenv 设置一个环境变量的值
