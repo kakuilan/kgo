549b1cda8eb65dc492c3f92f4300f1d5a3d25b47
kakuilan <kakuilan@163.com>
2019-11-07 10:13:49 +0800

    add CountBase64Byte

--- string.go
+++ string.go
@@ -1109,3 +1109,14 @@ func (ks *LkkString) HideTrueName(name string) string {
 
 	return res
 }
+
+// CountBase64Byte 统计base64字符串大小,字节.
+func (ks *LkkString) CountBase64Byte(str string) (res int) {
+	pos := strings.Index(str, ",")
+	if pos > 10 {
+		img := strings.Replace(str[pos:], "=", "", -1)
+		res = int(float64(len(img)) * float64(3.0/4.0))
+	}
+
+	return
+}
