26b2d0f2106762e389f6e0e8bf6f3e37db0c1f3d
kakuilan <kakuilan@163.com>
2019-11-08 10:54:14 +0800

    add ArraySearchItem

--- function.go
+++ function.go
@@ -237,3 +237,28 @@ func creditChecksum(id string) byte {
 
 	return code[sum%11]
 }
+
+// compareConditionMap 比对数组是否匹配条件.condition为条件字典,arr为要比对的数据数组.
+func compareConditionMap(condition map[string]interface{}, arr interface{}) (res interface{}) {
+	val := reflect.ValueOf(arr)
+	switch val.Kind() {
+	case reflect.Map:
+		condLen := len(condition)
+		chkNum := 0
+		if condLen > 0 {
+			for _, k := range val.MapKeys() {
+				if condVal, ok := condition[k.String()]; ok && reflect.DeepEqual(val.MapIndex(k).Interface(), condVal) {
+					chkNum++
+				}
+			}
+		}
+
+		if chkNum == condLen {
+			res = arr
+		}
+	default:
+		return
+	}
+
+	return
+}
