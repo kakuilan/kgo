4c008bb05ed7f569b09446f29e8be23c7f783762
kakuilan <kakuilan@163.com>
2020-03-07 23:30:37 +0800

    add KNum.RoundPlus

--- number.go
+++ number.go
@@ -146,9 +146,16 @@ func (kn *LkkNumber) Rand(min, max int) int {
 	return kn.RandInt(min, max)
 }
 
-// Round 对浮点数进行四舍五入.
+// Round 对浮点数(的整数)进行四舍五入.
 func (kn *LkkNumber) Round(value float64) float64 {
 	return math.Floor(value + 0.5)
+}
+
+// RoundPlus 对指定的小数位进行四舍五入.
+// precision为小数位数.
+func (kn *LkkNumber) RoundPlus(value float64, precision int8) float64 {
+	shift := math.Pow(10, float64(precision))
+	return kn.Round(value*shift) / shift
 }
 
 // Floor 向下取整.
