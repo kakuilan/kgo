50c15487cbda19540f6bfaa8c98034d8c41e3309
kakuilan <kakuilan@163.com>
2020-03-17 18:24:02 +0800

    fix KOS.GetCpuInfo

--- os.go
+++ os.go
@@ -669,59 +669,53 @@ func (ko *LkkOS) GetCpuInfo() *CpuInfo {
 
 	res.Threads = uint(runtime.NumCPU())
 	f, err := os.Open("/proc/cpuinfo")
-	if err != nil {
-		return res
-	}
-	defer func() {
-		_ = f.Close()
-	}()
+	if err == nil {
+		cpu := make(map[string]bool)
+		core := make(map[string]bool)
+		var cpuID string
 
-	cpu := make(map[string]bool)
-	core := make(map[string]bool)
-
-	var cpuID string
-
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		if sl := cpuRegTwoColumns.Split(s.Text(), 2); sl != nil {
-			switch sl[0] {
-			case "physical id":
-				cpuID = sl[1]
-				cpu[cpuID] = true
-			case "core id":
-				coreID := fmt.Sprintf("%s/%s", cpuID, sl[1])
-				core[coreID] = true
-			case "vendor_id":
-				if res.Vendor == "" {
-					res.Vendor = sl[1]
-				}
-			case "model name":
-				if res.Model == "" {
-					// CPU model, as reported by /proc/cpuinfo, can be a bit ugly. Clean up...
-					model := cpuRegExtraSpace.ReplaceAllLiteralString(sl[1], " ")
-					res.Model = strings.Replace(model, "- ", "-", 1)
-				}
-			case "cpu MHz":
-				if res.Speed == "" {
-					res.Speed = sl[1]
-				}
-			case "cache size":
-				if res.Cache == 0 {
-					if m := cpuRegCacheSize.FindStringSubmatch(sl[1]); m != nil {
-						if cache, err := strconv.ParseUint(m[1], 10, 64); err == nil {
-							res.Cache = uint(cache)
+		s := bufio.NewScanner(f)
+		for s.Scan() {
+			if sl := cpuRegTwoColumns.Split(s.Text(), 2); sl != nil {
+				switch sl[0] {
+				case "physical id":
+					cpuID = sl[1]
+					cpu[cpuID] = true
+				case "core id":
+					coreID := fmt.Sprintf("%s/%s", cpuID, sl[1])
+					core[coreID] = true
+				case "vendor_id":
+					if res.Vendor == "" {
+						res.Vendor = sl[1]
+					}
+				case "model name":
+					if res.Model == "" {
+						// CPU model, as reported by /proc/cpuinfo, can be a bit ugly. Clean up...
+						model := cpuRegExtraSpace.ReplaceAllLiteralString(sl[1], " ")
+						res.Model = strings.Replace(model, "- ", "-", 1)
+					}
+				case "cpu MHz":
+					if res.Speed == "" {
+						res.Speed = sl[1]
+					}
+				case "cache size":
+					if res.Cache == 0 {
+						if m := cpuRegCacheSize.FindStringSubmatch(sl[1]); m != nil {
+							if cache, err := strconv.ParseUint(m[1], 10, 64); err == nil {
+								res.Cache = uint(cache)
+							}
 						}
 					}
 				}
 			}
 		}
-	}
-	if s.Err() != nil {
-		return res
-	}
 
-	res.Cpus = uint(len(cpu))
-	res.Cores = uint(len(core))
+		res.Cpus = uint(len(cpu))
+		res.Cores = uint(len(core))
+	}
+	defer func() {
+		_ = f.Close()
+	}()
 
 	return res
 }
