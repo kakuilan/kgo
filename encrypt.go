944eb77709d606ad0855f5001f68f62718b60972
kakuilan <kakuilan@163.com>
2020-06-20 19:19:59 +0800

    fix Base64Encode,Base64Decode,Base64UrlEncode,Base64UrlDecode,AuthCode,EasyEncrypt,EasyDecrypt,HmacShaX 输入和输出为[]byte

--- encrypt.go
+++ encrypt.go
@@ -22,109 +22,111 @@ import (
 	"io"
 	"math/big"
 	"strconv"
-	"strings"
 	"time"
 )
 
 // Base64Encode 使用 MIME base64 对数据进行编码.
-func (ke *LkkEncrypt) Base64Encode(str []byte) string {
-	return base64.StdEncoding.EncodeToString(str)
+func (ke *LkkEncrypt) Base64Encode(str []byte) []byte {
+	buf := make([]byte, base64.StdEncoding.EncodedLen(len(str)))
+	base64.StdEncoding.Encode(buf, str)
+	return buf
 }
 
 // Base64Decode 对使用 MIME base64 编码的数据进行解码.
-func (ke *LkkEncrypt) Base64Decode(str string) ([]byte, error) {
-	switch len(str) % 4 {
-	case 2:
-		str += "=="
-	case 3:
-		str += "="
-	}
-
-	data, err := base64.StdEncoding.DecodeString(str)
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
+func (ke *LkkEncrypt) Base64Decode(str []byte) ([]byte, error) {
+	dbuf := make([]byte, base64.StdEncoding.DecodedLen(len(str)))
+	n, err := base64.StdEncoding.Decode(dbuf, str)
+	return dbuf[:n], err
 }
 
 // Base64UrlSafeEncode url安全的Base64Encode,没有'/'和'+'及结尾的'=' .
-func (ke *LkkEncrypt) Base64UrlEncode(source []byte) string {
+func (ke *LkkEncrypt) Base64UrlEncode(source []byte) []byte {
+	buf := make([]byte, base64.StdEncoding.EncodedLen(len(source)))
+	base64.StdEncoding.Encode(buf, source)
+
 	// Base64 Url Safe is the same as Base64 but does not contain '/' and '+' (replaced by '_' and '-') and trailing '=' are removed.
-	bytearr := base64.StdEncoding.EncodeToString(source)
-	safeurl := strings.Replace(string(bytearr), "/", "_", -1)
-	safeurl = strings.Replace(safeurl, "+", "-", -1)
-	safeurl = strings.Replace(safeurl, "=", "", -1)
-	return safeurl
+	buf = bytes.Replace(buf, []byte("/"), []byte("_"), -1)
+	buf = bytes.Replace(buf, []byte("+"), []byte("-"), -1)
+	buf = bytes.Replace(buf, []byte("="), []byte(""), -1)
+
+	return buf
 }
 
 // Base64UrlDecode url安全的Base64Decode.
-func (ke *LkkEncrypt) Base64UrlDecode(data string) ([]byte, error) {
+func (ke *LkkEncrypt) Base64UrlDecode(data []byte) ([]byte, error) {
 	var missing = (4 - len(data)%4) % 4
-	data += strings.Repeat("=", missing)
-	return base64.URLEncoding.DecodeString(data)
+	data = append(data, bytes.Repeat([]byte("="), missing)...)
+
+	dbuf := make([]byte, base64.URLEncoding.DecodedLen(len(data)))
+	n, err := base64.URLEncoding.Decode(dbuf, data)
+	return dbuf[:n], err
 }
 
 // AuthCode 授权码编码或解码;encode为true时编码,为false解码;expiry为有效期,秒;返回结果为加密/解密的字符串和有效期时间戳.
-func (ke *LkkEncrypt) AuthCode(str, key string, encode bool, expiry int64) (string, int64) {
+func (ke *LkkEncrypt) AuthCode(str, key []byte, encode bool, expiry int64) ([]byte, int64) {
 	// DYNAMIC_KEY_LEN 动态密钥长度，相同的明文会生成不同密文就是依靠动态密钥
 	// 加入随机密钥，可以令密文无任何规律，即便是原文和密钥完全相同，加密结果也会每次不同，增大破解难度。
 	// 取值越大，密文变动规律越大，密文变化 = 16 的 DYNAMIC_KEY_LEN 次方
 	// 当此值为 0 时，则不产生随机密钥
 
-	if str == "" {
-		return "", 0
-	} else if !encode && len(str) < DYNAMIC_KEY_LEN {
-		return "", 0
+	strLen := len(str)
+	if str == nil || strLen == 0 {
+		return nil, 0
+	} else if !encode && strLen < DYNAMIC_KEY_LEN {
+		return nil, 0
 	}
 
 	// 密钥
-	keyByte := md5Str([]byte(key), 32)
+	keyByte := md5Str(key, 32)
 
 	// 密钥a会参与加解密
-	keya := md5Str(keyByte[:16], 32)
+	keya := keyByte[:16]
 
 	// 密钥b会用来做数据完整性验证
-	keyb := md5Str(keyByte[16:], 32)
+	keyb := keyByte[16:]
 
 	// 密钥c用于变化生成的密文
 	var keyc []byte
 	if encode == false {
-		keyc = []byte(str[:DYNAMIC_KEY_LEN])
+		keyc = str[:DYNAMIC_KEY_LEN]
 	} else {
-		cLen := 32 - DYNAMIC_KEY_LEN
 		now, _ := time.Now().MarshalBinary()
+		keycLen := 32 - DYNAMIC_KEY_LEN
 		timeBytes := md5Str(now, 32)
-		keyc = timeBytes[cLen:]
+		keyc = timeBytes[keycLen:]
 	}
 
 	// 参与运算的密钥
 	keyd := md5Str(append(keya, keyc...), 32)
 	cryptkey := append(keya, keyd...)
-	keyLength := len(cryptkey)
+	cryptkeyLen := len(cryptkey)
 	// 明文，前10位用来保存时间戳，解密时验证数据有效性，10到26位用来保存keyb(密钥b)，解密时会通过这个密钥验证数据完整性
 	// 如果是解码的话，会从第 DYNAMIC_KEY_LEN 位开始，因为密文前 DYNAMIC_KEY_LEN 位保存 动态密钥，以保证解密正确
-	if encode == false {
-		strByte, err := ke.Base64UrlDecode(str[DYNAMIC_KEY_LEN:])
+	if encode == false { //解密
+		var err error
+		str, err = ke.Base64UrlDecode(str[DYNAMIC_KEY_LEN:])
 		if err != nil {
-			return "", 0
+			return nil, 0
 		}
-		str = string(strByte)
 	} else {
 		if expiry != 0 {
 			expiry = expiry + time.Now().Unix()
 		}
-		expMd5 := md5Str(append([]byte(str), keyb...), 16)
-		str = fmt.Sprintf("%010d%s%s", expiry, expMd5, str)
+		expMd5 := md5Str(append(str, keyb...), 16)
+		str = []byte(fmt.Sprintf("%010d%s%s", expiry, expMd5, str))
+		//str = append([]byte(fmt.Sprintf("%010d", expiry)), append(expMd5, str...)...)
 	}
-	stringLength := len(str)
-	resdata := make([]byte, 0, stringLength)
+
+	strLen = len(str)
+	resdata := make([]byte, 0, strLen)
 	var rndkey, box [256]int
 	// 产生密钥簿
-	j := 0
-	a := 0
+	h := 0
 	i := 0
+	j := 0
+
 	for i = 0; i < 256; i++ {
-		rndkey[i] = int(cryptkey[i%keyLength])
+		rndkey[i] = int(cryptkey[i%cryptkeyLen])
 		box[i] = i
 	}
 	// 用固定的算法，打乱密钥簿，增加随机性，好像很复杂，实际上并不会增加密文的强度
@@ -133,35 +135,34 @@ func (ke *LkkEncrypt) AuthCode(str, key string, encode bool, expiry int64) (stri
 		box[i], box[j] = box[j], box[i]
 	}
 	// 核心加解密部分
-	a = 0
+	h = 0
 	j = 0
-	for i = 0; i < stringLength; i++ {
-		a = ((a + 1) % 256)
-		j = ((j + box[a]) % 256)
-		box[a], box[j] = box[j], box[a]
+	for i = 0; i < strLen; i++ {
+		h = ((h + 1) % 256)
+		j = ((j + box[h]) % 256)
+		box[h], box[j] = box[j], box[h]
 		// 从密钥簿得出密钥进行异或，再转成字符
-		resdata = append(resdata, byte(int(str[i])^box[(box[a]+box[j])%256]))
+		resdata = append(resdata, byte(int(str[i])^box[(box[h]+box[j])%256]))
 	}
-	result := string(resdata)
 	if encode == false { //解密
 		// substr($result, 0, 10) == 0 验证数据有效性
 		// substr($result, 0, 10) - time() > 0 验证数据有效性
 		// substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16) 验证数据完整性
 		// 验证数据有效性，请看未加密明文的格式
-		if len(result) <= 26 {
-			return "", 0
+		if len(resdata) <= 26 {
+			return nil, 0
 		}
 
-		expTime, _ := strconv.ParseInt(result[:10], 10, 0)
-		if (expTime == 0 || expTime-time.Now().Unix() > 0) && result[10:26] == string(md5Str(append(resdata[26:], keyb...), 16)) {
-			return result[26:], expTime
+		expTime, _ := strconv.ParseInt(string(resdata[:10]), 10, 0)
+		if (expTime == 0 || expTime-time.Now().Unix() > 0) && string(resdata[10:26]) == string(md5Str(append(resdata[26:], keyb...), 16)) {
+			return resdata[26:], expTime
 		} else {
-			return "", expTime
+			return nil, expTime
 		}
 	} else { //加密
 		// 把动态密钥保存在密文里，这也是为什么同样的明文，生产不同密文后能解密的原因
-		result = string(keyc) + ke.Base64UrlEncode(resdata)
-		return result, expiry
+		resdata = append(keyc, ke.Base64UrlEncode(resdata)...)
+		return resdata, expiry
 	}
 }
 
@@ -179,8 +180,8 @@ func (ke *LkkEncrypt) PasswordHash(password []byte, costs ...int) ([]byte, error
 		}
 	}
 
-	bytes, err := bcrypt.GenerateFromPassword(password, cost)
-	return bytes, err
+	res, err := bcrypt.GenerateFromPassword(password, cost)
+	return res, err
 }
 
 // PasswordVerify 验证密码是否和散列值匹配.
@@ -191,54 +192,54 @@ func (ke *LkkEncrypt) PasswordVerify(password, hash []byte) bool {
 
 // EasyEncrypt 简单加密.
 // data为要加密的原字符串,key为密钥.
-func (ke *LkkEncrypt) EasyEncrypt(data, key string) string {
+func (ke *LkkEncrypt) EasyEncrypt(data, key []byte) []byte {
 	dataLen := len(data)
 	if dataLen == 0 {
-		return ""
+		return nil
 	}
 
-	keyByte := md5Str([]byte(key), 32)
+	keyByte := md5Str(key, 32)
 	keyLen := len(keyByte)
 
 	var i, x, c int
-	var str []byte
+	var res []byte
 	for i = 0; i < dataLen; i++ {
 		if x == keyLen {
 			x = 0
 		}
 
 		c = (int(data[i]) + int(keyByte[x])) % 256
-		str = append(str, byte(c))
+		res = append(res, byte(c))
 
 		x++
 	}
 
-	res := string(keyByte[:DYNAMIC_KEY_LEN]) + ke.Base64UrlEncode(str)
+	res = append(keyByte[:DYNAMIC_KEY_LEN], ke.Base64UrlEncode(res)...)
 	return res
 }
 
 // EasyDecrypt 简单解密.
 // val为待解密的字符串,key为密钥.
-func (ke *LkkEncrypt) EasyDecrypt(val, key string) string {
+func (ke *LkkEncrypt) EasyDecrypt(val, key []byte) []byte {
 	if len(val) <= DYNAMIC_KEY_LEN {
-		return ""
+		return nil
 	}
 
 	data, err := ke.Base64UrlDecode(val[DYNAMIC_KEY_LEN:])
 	if err != nil {
-		return ""
+		return nil
 	}
 
-	keyByte := md5Str([]byte(key), 32)
-	if val[:DYNAMIC_KEY_LEN] != string(keyByte[:DYNAMIC_KEY_LEN]) {
-		return ""
+	keyByte := md5Str(key, 32)
+	if string(val[:DYNAMIC_KEY_LEN]) != string(keyByte[:DYNAMIC_KEY_LEN]) {
+		return nil
 	}
 
 	dataLen := len(data)
 	keyLen := len(keyByte)
 
 	var i, x, c int
-	var str []byte
+	var res []byte
 	for i = 0; i < dataLen; i++ {
 		if x == keyLen {
 			x = 0
@@ -249,16 +250,16 @@ func (ke *LkkEncrypt) EasyDecrypt(val, key string) string {
 		} else {
 			c = int(data[i]) - int(keyByte[x])
 		}
-		str = append(str, byte(c))
+		res = append(res, byte(c))
 
 		x++
 	}
 
-	return string(str)
+	return res
 }
 
 // HmacShaX HmacSHA-x加密,x为1/256/512 .
-func (ke *LkkEncrypt) HmacShaX(data, secret []byte, x uint16) string {
+func (ke *LkkEncrypt) HmacShaX(data, secret []byte, x uint16) []byte {
 	// Create a new HMAC by defining the hash type and the key (as byte array)
 	var h hash.Hash
 	switch x {
@@ -278,10 +279,11 @@ func (ke *LkkEncrypt) HmacShaX(data, secret []byte, x uint16) string {
 	// Write Data to it
 	h.Write(data)
 
-	// Get result and encode as hexadecimal string
-	sha := hex.EncodeToString(h.Sum(nil))
+	src := h.Sum(nil)
+	dst := make([]byte, hex.EncodedLen(len(src)))
+	hex.Encode(dst, src)
 
-	return sha
+	return dst
 }
 
 // aesEncrypt AES加密.
