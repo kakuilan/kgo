50ae5ddad8755105e67a228bae103475f3975e09
kakuilan <kakuilan@163.com>
2020-06-20 19:19:59 +0800

    fix Base64Encode,Base64Decode,Base64UrlEncode,Base64UrlDecode,AuthCode,EasyEncrypt,EasyDecrypt,HmacShaX 输入和输出为[]byte

--- encrypt_test.go
+++ encrypt_test.go
@@ -10,7 +10,7 @@ import (
 func TestBase64Encode(t *testing.T) {
 	str := []byte("This is an string to encod")
 	res := KEncr.Base64Encode(str)
-	if !strings.HasSuffix(res, "=") {
+	if !strings.HasSuffix(string(res), "=") {
 		t.Error("Base64Encode fail")
 		return
 	}
@@ -25,24 +25,25 @@ func BenchmarkBase64Encode(b *testing.B) {
 }
 
 func TestBase64Decode(t *testing.T) {
-	str := "VGhpcyBpcyBhbiBlbmNvZGVkIHN0cmluZw=="
+	str := []byte("VGhpcyBpcyBhbiBlbmNvZGVkIHN0cmluZw==")
 	_, err := KEncr.Base64Decode(str)
 	if err != nil {
 		t.Error("Base64Decode fail")
 		return
 	}
-	_, err = KEncr.Base64Decode("#iu3498r")
+
+	_, err = KEncr.Base64Decode([]byte("#iu3498r"))
 	if err == nil {
 		t.Error("Base64Decode fail")
 		return
 	}
-	_, err = KEncr.Base64Decode("VGhpcy")
-	_, err = KEncr.Base64Decode("VGhpcyB")
+	_, err = KEncr.Base64Decode([]byte("VGhpcy"))
+	_, err = KEncr.Base64Decode([]byte("VGhpcyB"))
 }
 
 func BenchmarkBase64Decode(b *testing.B) {
 	b.ResetTimer()
-	str := "VGhpcyBpcyBhbiBlbmNvZGVkIHN0cmluZw=="
+	str := []byte("VGhpcyBpcyBhbiBlbmNvZGVkIHN0cmluZw==")
 	for i := 0; i < b.N; i++ {
 		_, _ = KEncr.Base64Decode(str)
 	}
@@ -51,13 +52,13 @@ func BenchmarkBase64Decode(b *testing.B) {
 func TestBase64UrlEncodeDecode(t *testing.T) {
 	str := []byte("This is an string to encod")
 	res := KEncr.Base64UrlEncode(str)
-	if strings.HasSuffix(res, "=") {
+	if strings.HasSuffix(string(res), "=") {
 		t.Error("Base64UrlEncode fail")
 		return
 	}
 
-	_, err := KEncr.Base64UrlDecode(res)
-	if err != nil {
+	ret, err := KEncr.Base64UrlDecode(res)
+	if err != nil || string(ret) != string(str) {
 		t.Error("Base64UrlDecode fail")
 		return
 	}
@@ -73,42 +74,42 @@ func BenchmarkBase64UrlEncode(b *testing.B) {
 
 func BenchmarkBase64UrlDecode(b *testing.B) {
 	b.ResetTimer()
-	str := "VGhpcyBpcyBhbiBzdHJpbmcgdG8gZW5jb2Q"
+	str := []byte("VGhpcyBpcyBhbiBzdHJpbmcgdG8gZW5jb2Q")
 	for i := 0; i < b.N; i++ {
 		_, _ = KEncr.Base64UrlDecode(str)
 	}
 }
 
 func TestAuthCode(t *testing.T) {
-	key := "123456"
-	str := "hello world"
+	key := []byte("123456")
+	str := []byte("hello world")
 
 	res, _ := KEncr.AuthCode(str, key, true, 0)
-	if res == "" {
+	if res == nil {
 		t.Error("AuthCode Encode fail")
 		return
 	}
 
 	res2, _ := KEncr.AuthCode(res, key, false, 0)
-	if res2 == "" {
+	if res2 == nil || string(res2) != string(str) {
 		t.Error("AuthCode Decode fail")
 		return
 	}
 
 	res, _ = KEncr.AuthCode(str, key, true, -3600)
 	KEncr.AuthCode(res, key, false, 0)
-	KEncr.AuthCode("", key, true, 0)
-	KEncr.AuthCode("", "", true, 0)
-	KEncr.AuthCode("7caeNfPt/N1zHdj5k/7i7pol6NHsVs0Cji7c15h4by1RYcrBoo7EEw==", key, false, 0)
-	KEncr.AuthCode("7caeNfPt/N1zHdj5k/7i7pol6N", key, false, 0)
-	KEncr.AuthCode("123456", "", false, 0)
-	KEncr.AuthCode("1234#iu3498r", "", false, 0)
+	KEncr.AuthCode([]byte(""), key, true, 0)
+	KEncr.AuthCode([]byte(""), []byte(""), true, 0)
+	KEncr.AuthCode([]byte("7caeNfPt/N1zHdj5k/7i7pol6NHsVs0Cji7c15h4by1RYcrBoo7EEw=="), key, false, 0)
+	KEncr.AuthCode([]byte("7caeNfPt/N1zHdj5k/7i7pol6N"), key, false, 0)
+	KEncr.AuthCode([]byte("123456"), []byte(""), false, 0)
+	KEncr.AuthCode([]byte("1234#iu3498r"), []byte(""), false, 0)
 }
 
 func BenchmarkAuthCodeEncode(b *testing.B) {
 	b.ResetTimer()
-	key := "123456"
-	str := "hello world"
+	key := []byte("123456")
+	str := []byte("hello world")
 	for i := 0; i < b.N; i++ {
 		KEncr.AuthCode(str, key, true, 0)
 	}
@@ -116,10 +117,89 @@ func BenchmarkAuthCodeEncode(b *testing.B) {
 
 func BenchmarkAuthCodeDecode(b *testing.B) {
 	b.ResetTimer()
-	key := "123456"
-	str := "a79b5do3C9nbaZsAz5j3NQRj4e/L6N+y5fs2U9r1mO0LinOWtxmscg=="
+	key := []byte("123456")
+	str := []byte("471911e9DQfGWbM-TvbP6eTsOGZmtdA8wja_CuYN0ynfuPau9dODVwnjVg")
 	for i := 0; i < b.N; i++ {
 		KEncr.AuthCode(str, key, false, 0)
+	}
+}
+
+func TestEasyEncryptDecrypt(t *testing.T) {
+	key := []byte("123456")
+	str := []byte("hello world你好!hello world你好!hello world你好!hello world你好!")
+	enc := KEncr.EasyEncrypt(str, key)
+	if enc == nil {
+		t.Error("EasyEncrypt fail")
+		return
+	}
+
+	dec := KEncr.EasyDecrypt(enc, key)
+	if string(dec) != string(str) {
+		t.Error("EasyDecrypt fail")
+		return
+	}
+
+	dec = KEncr.EasyDecrypt([]byte("你好，世界！"), key)
+	if dec != nil {
+		t.Error("EasyDecrypt fail")
+		return
+	}
+
+	KEncr.EasyEncrypt([]byte(""), key)
+	KEncr.EasyEncrypt([]byte(""), []byte(""))
+	KEncr.EasyDecrypt(enc, []byte("1qwer"))
+	KEncr.EasyDecrypt([]byte("123"), key)
+	KEncr.EasyDecrypt([]byte("1234#iu3498r"), key)
+}
+
+func BenchmarkEasyEncrypt(b *testing.B) {
+	b.ResetTimer()
+	key := []byte("123456")
+	str := []byte("hello world你好")
+	for i := 0; i < b.N; i++ {
+		KEncr.EasyEncrypt(str, key)
+	}
+}
+
+func BenchmarkEasyDecrypt(b *testing.B) {
+	b.ResetTimer()
+	key := []byte("123456")
+	str := []byte("e10adc39zZaczdODqqimpcZF8tlGBx8")
+	for i := 0; i < b.N; i++ {
+		KEncr.EasyDecrypt(str, key)
+	}
+}
+
+func TestHmacShaX(t *testing.T) {
+	str := []byte("hello world")
+	key := []byte("123456")
+	res1 := KEncr.HmacShaX(str, key, 1)
+	res2 := KEncr.HmacShaX(str, key, 256)
+	res3 := KEncr.HmacShaX(str, key, 512)
+	if res1 == nil || res2 == nil || res3 == nil {
+		t.Error("HmacShaX fail")
+		return
+	}
+}
+
+func TestHmacShaXPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recover...:", r)
+		}
+	}()
+
+	str := []byte("hello world")
+	key := []byte("123456")
+	KEncr.HmacShaX(str, key, 4)
+}
+
+func BenchmarkHmacShaX(b *testing.B) {
+	b.ResetTimer()
+	str := []byte("hello world")
+	key := []byte("123456")
+	for i := 0; i < b.N; i++ {
+		KEncr.HmacShaX(str, key, 256)
 	}
 }
 
@@ -165,85 +245,6 @@ func BenchmarkPasswordVerify(b *testing.B) {
 			break
 		}
 		KEncr.PasswordVerify(pwd, has)
-	}
-}
-
-func TestEasyEncryptDecrypt(t *testing.T) {
-	key := "123456"
-	str := "hello world你好!hello world你好!hello world你好!hello world你好!"
-	enc := KEncr.EasyEncrypt(str, key)
-	if enc == "" {
-		t.Error("EasyEncrypt fail")
-		return
-	}
-
-	dec := KEncr.EasyDecrypt(enc, key)
-	if dec != str {
-		t.Error("EasyDecrypt fail")
-		return
-	}
-
-	dec = KEncr.EasyDecrypt("你好，世界！", key)
-	if dec != "" {
-		t.Error("EasyDecrypt fail")
-		return
-	}
-
-	KEncr.EasyEncrypt("", key)
-	KEncr.EasyEncrypt("", "")
-	KEncr.EasyDecrypt(enc, "1qwer")
-	KEncr.EasyDecrypt("123", key)
-	KEncr.EasyDecrypt("1234#iu3498r", key)
-}
-
-func BenchmarkEasyEncrypt(b *testing.B) {
-	b.ResetTimer()
-	key := "123456"
-	str := "hello world你好"
-	for i := 0; i < b.N; i++ {
-		KEncr.EasyEncrypt(str, key)
-	}
-}
-
-func BenchmarkEasyDecrypt(b *testing.B) {
-	b.ResetTimer()
-	key := "123456"
-	str := "e10azZaczdODqqimpcY"
-	for i := 0; i < b.N; i++ {
-		KEncr.EasyDecrypt(str, key)
-	}
-}
-
-func TestHmacShaX(t *testing.T) {
-	str := []byte("hello world")
-	key := []byte("123456")
-	res1 := KEncr.HmacShaX(str, key, 1)
-	res2 := KEncr.HmacShaX(str, key, 256)
-	res3 := KEncr.HmacShaX(str, key, 512)
-	if res1 == "" || res2 == "" || res3 == "" {
-		t.Error("HmacShaX fail")
-		return
-	}
-}
-
-func TestHmacShaXPanic(t *testing.T) {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("recover...:", r)
-		}
-	}()
-
-	str := []byte("hello world")
-	key := []byte("123456")
-	KEncr.HmacShaX(str, key, 4)
-}
-
-func BenchmarkHmacShaX(b *testing.B) {
-	b.ResetTimer()
-	str := []byte("hello world")
-	key := []byte("123456")
-	for i := 0; i < b.N; i++ {
-		KEncr.HmacShaX(str, key, 256)
 	}
 }
 
