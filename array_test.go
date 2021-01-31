df46438f23af1386c7953e0165025757adb5b876
kakuilan <kakuilan@163.com>
2021-01-31 15:26:39 +0800

    add TestArray_ArrayChunk_PanicType

--- array_test.go
+++ array_test.go
@@ -18,12 +18,20 @@ func TestArray_ArrayChunk(t *testing.T) {
 	KArr.ArrayChunk([]int{}, 1)
 }
 
-func TestArray_ArrayChunk_Panic(t *testing.T) {
+func TestArray_ArrayChunk_PanicSize(t *testing.T) {
 	defer func() {
 		r := recover()
 		assert.Equal(t, "[ArrayChunk]`size cannot be less than 1", r)
 	}()
 	KArr.ArrayChunk(strSli, 0)
+}
+
+func TestArray_ArrayChunk_PanicType(t *testing.T) {
+	defer func() {
+		r := recover()
+		assert.Equal(t, "[ArrayChunk]`arr type must be array or slice", r)
+	}()
+	KArr.ArrayChunk("hello", 2)
 }
 
 func BenchmarkArray_ArrayChunk(b *testing.B) {
