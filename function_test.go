71a1ce3b28d2c81293b62fa18c89d3db457f6924
kakuilan <kakuilan@163.com>
2021-01-31 12:13:50 +0800

    add md5Byte test

---
+++ function_test.go
@@ -0,0 +1,33 @@
+package kgo
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func init() {
+	gofakeit.Seed(0)
+}
+
+var randomName = gofakeit.Name()
+
+func TestFun_md5Byte(t *testing.T) {
+	println(randomName)
+
+	res1 := md5Byte([]byte(""), 32)
+	res2 := md5Byte([]byte(randomName), 24)
+	res3 := md5Byte([]byte(randomName), 0)
+	res4 := md5Byte([]byte(randomName), 64)
+
+	assert.Equal(t, 32, len(res1))
+	assert.Equal(t, 24, len(res2))
+	assert.NotEqual(t, 0, len(res3))
+	assert.NotEqual(t, 64, len(res4))
+}
+
+func BenchmarkFun_md5Byte(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = md5Byte([]byte(randomName), 32)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kakuilan/kgo
 
 go 1.15
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/brianvoe/gofakeit/v6 v6.0.2
+	github.com/stretchr/testify v1.7.0
+)
