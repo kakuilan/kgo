d63b53ab3a3a857d5738f4663da2b569eac61d2d
kakuilan <kakuilan@163.com>
2021-01-31 12:13:34 +0800

    add func md5Byte

---
+++ function.go
@@ -0,0 +1,24 @@
+package kgo
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// md5Byte 计算字节切片的 MD5 散列值.
+func md5Byte(str []byte, length uint8) []byte {
+	var res []byte
+	h := md5.New()
+	h.Write(str)
+
+	hBytes := h.Sum(nil)
+	dst := make([]byte, hex.EncodedLen(len(hBytes)))
+	hex.Encode(dst, hBytes)
+	if length > 0 && length < 32 {
+		res = dst[:length]
+	} else {
+		res = dst
+	}
+
+	return res
+}
