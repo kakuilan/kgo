ebceeaff0a1abdb9bf8e71b142b1870bc92e2a3c
kakuilan <kakuilan@163.com>
2021-02-02 23:37:36 +0800

    add isArrayOrSlice/isMap/isStruct

--- function.go
+++ function.go
@@ -7,6 +7,39 @@ import (
 	"reflect"
 )
 
+// isArrayOrSlice 检查变量是否数组或切片.
+// chkType为检查类型,枚举值有(1仅数组,2仅切片,3数组或切片);结果为-1表示非,>=0表示是(数组长度).
+func isArrayOrSlice(val interface{}, chkType uint8) int {
+	if chkType != 1 && chkType != 2 && chkType != 3 {
+		panic(fmt.Sprintf("[isArrayOrSlice]`chkType refValue muset in [1, 2, 3]; but: %d", chkType))
+	}
+
+	var res = -1
+	refVal := reflect.ValueOf(val)
+	switch refVal.Kind() {
+	case reflect.Array:
+		if chkType == 1 || chkType == 3 {
+			res = refVal.Len()
+		}
+	case reflect.Slice:
+		if chkType == 2 || chkType == 3 {
+			res = refVal.Len()
+		}
+	}
+
+	return res
+}
+
+// isMap 检查变量是否字典.
+func isMap(val interface{}) bool {
+	return reflect.ValueOf(val).Kind() == reflect.Map
+}
+
+// isStruct 检查变量是否结构体.
+func isStruct(val interface{}) bool {
+	return reflect.ValueOf(val).Kind() == reflect.Struct
+}
+
 // md5Byte 计算字节切片的 MD5 散列值.
 func md5Byte(str []byte, length uint8) []byte {
 	var res []byte
