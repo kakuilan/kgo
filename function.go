8a11e199da65c84f1737ee7fa86ca962a6a422f5
kakuilan <kakuilan@163.com>
2021-02-03 10:19:30 +0800

    add isInt/isFloat/isNumeric

--- function.go
+++ function.go
@@ -5,13 +5,14 @@ import (
 	"encoding/hex"
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 // isArrayOrSlice 检查变量是否数组或切片.
 // chkType为检查类型,枚举值有(1仅数组,2仅切片,3数组或切片);结果为-1表示非,>=0表示是(数组长度).
 func isArrayOrSlice(val interface{}, chkType uint8) int {
 	if chkType != 1 && chkType != 2 && chkType != 3 {
-		panic(fmt.Sprintf("[isArrayOrSlice]`chkType refValue muset in [1, 2, 3]; but: %d", chkType))
+		panic(fmt.Sprintf("[isArrayOrSlice]`chkType refValue must in [1, 2, 3]; but: %d", chkType))
 	}
 
 	var res = -1
@@ -38,6 +39,61 @@ func isMap(val interface{}) bool {
 // isStruct 检查变量是否结构体.
 func isStruct(val interface{}) bool {
 	return reflect.ValueOf(val).Kind() == reflect.Struct
+}
+
+// isInt 变量是否整型数值.
+func isInt(val interface{}) bool {
+	switch val.(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return true
+	case string:
+		str := val.(string)
+		if str == "" {
+			return false
+		}
+		_, err := strconv.Atoi(str)
+		return err == nil
+	}
+
+	return false
+}
+
+// isFloat 变量是否浮点数值.
+func isFloat(val interface{}) bool {
+	switch val.(type) {
+	case float32, float64:
+		return true
+	case string:
+		str := val.(string)
+		if str == "" {
+			return false
+		}
+
+		if ok := RegFloat.MatchString(str); ok {
+			return true
+		}
+	}
+
+	return false
+}
+
+// isNumeric 变量是否数值(不包含复数和科学计数法).
+func isNumeric(val interface{}) bool {
+	switch val.(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return true
+	case float32, float64:
+		return true
+	case string:
+		str := val.(string)
+		if str == "" {
+			return false
+		}
+		_, err := strconv.ParseFloat(str, 64)
+		return err == nil
+	}
+
+	return false
 }
 
 // md5Byte 计算字节切片的 MD5 散列值.
