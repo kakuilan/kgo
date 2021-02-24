c5d8b99772954e692f072aa0a269fd2a7895a72a
kakuilan <kakuilan@163.com>
2021-02-24 11:58:04 +0800

    add KArr.JoinInts

--- array.go
+++ array.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
@@ -235,6 +236,25 @@ func (ka *LkkArray) JoinStrings(delimiter string, strs []string) (res string) {
 	var sb strings.Builder
 	for _, str := range strs {
 		sb.WriteString(str)
+		if length--; length > 0 {
+			sb.WriteString(delimiter)
+		}
+	}
+	res = sb.String()
+
+	return
+}
+
+// JoinInts 使用分隔符delimiter连接整数切片.
+func (ka *LkkArray) JoinInts(delimiter string, ints []int) (res string) {
+	length := len(ints)
+	if length == 0 {
+		return
+	}
+
+	var sb strings.Builder
+	for _, num := range ints {
+		sb.WriteString(strconv.Itoa(num))
 		if length--; length > 0 {
 			sb.WriteString(delimiter)
 		}
--- array_test.go
+++ array_test.go
@@ -317,3 +317,17 @@ func BenchmarkArray_JoinStrings(b *testing.B) {
 		KArr.JoinStrings(",", ssSingle)
 	}
 }
+
+func TestArray_JoinInts(t *testing.T) {
+	ints := naturalArr[:]
+	res := KArr.JoinInts(",", ints)
+	assert.Contains(t, res, "0,1,2,3,4,5,6,7,8,9,10")
+}
+
+func BenchmarkArray_JoinInts(b *testing.B) {
+	b.ResetTimer()
+	ints := naturalArr[:]
+	for i := 0; i < b.N; i++ {
+		KArr.JoinInts(",", ints)
+	}
+}
