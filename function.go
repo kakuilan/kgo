69e932fe9c9d5c07043766c5d783938365fc46e0
kakuilan <kakuilan@163.com>
2021-03-22 16:26:58 +0800

    fix pkcs7Padding

--- function.go
+++ function.go
@@ -652,13 +652,14 @@ func pkcs7Padding(cipherText []byte, blockSize int, isZero bool) []byte {
 // pkcs7UnPadding PKCS7拆解.
 // origData为源数据;blockSize为分组长度.
 func pkcs7UnPadding(origData []byte, blockSize int) []byte {
+	origData = zeroUnPadding(origData)
 	olen := len(origData)
 	if origData == nil || olen == 0 || blockSize <= 0 || olen%blockSize != 0 {
 		return nil
 	}
 
 	unpadding := int(origData[olen-1])
-	if unpadding == 0 || unpadding > olen {
+	if unpadding > olen {
 		return nil
 	}
 
