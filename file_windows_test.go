5651aca43d3e62de482a276ba6278fa23237a1d5
kakuilan <kakuilan@163.com>
2021-04-09 11:20:06 +0800

    fix file_windows_test.go

--- file_windows_test.go
+++ file_windows_test.go
@@ -24,19 +24,3 @@ func TestFileWins_IsExecutable_Deny(t *testing.T) {
 	res = KFile.IsExecutable(admDir)
 	assert.False(t, res)
 }
-
-func TestFileWins_TarGzUnTarGz(t *testing.T) {
-	var res1, res2 bool
-	var err1, err2 error
-
-	//打包无权限的目录
-	res1, err1 = KFile.TarGz(admTesDir, targzfile1)
-	assert.False(t, res1)
-	assert.NotNil(t, err1)
-
-	//解压到无权限的目录
-	res2, err2 = KFile.UnTarGz(targzfile1, admDir)
-	dumpPrint(res2, err2)
-	assert.False(t, res2)
-	assert.NotNil(t, err2)
-}
