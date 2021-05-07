109e3051054551c26a9b4414952e5daf2d159276
kakuilan <kakuilan@163.com>
2021-05-07 19:56:59 +0800

    del url.go

--- string_test.go
+++ string_test.go
@@ -2,6 +2,7 @@ package kgo
 
 import (
 	"github.com/stretchr/testify/assert"
+	"net/url"
 	"testing"
 )
 
@@ -338,5 +339,360 @@ func BenchmarkString_Html2Text(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		KStr.Html2Text(tesHtmlDoc)
+	}
+}
+
+func TestString_ParseStr(t *testing.T) {
+	var res map[string]interface{}
+	var err error
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri1, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri2, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri3, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri4, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri5, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri6, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri7, res)
+	assert.Nil(t, err)
+
+	//将不合法的参数名转换
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri8, res)
+	assert.Nil(t, err)
+
+	//错误的
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri9, res)
+	assert.NotNil(t, err)
+
+	//错误的
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri10, res)
+	assert.NotNil(t, err)
+
+	//错误的
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri11, res)
+	assert.NotNil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri12, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri13, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri14, res)
+	assert.NotNil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri15, res)
+	assert.NotNil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri16, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri17, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri18, res)
+	assert.NotNil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri19, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri20, res)
+	assert.Nil(t, err)
+
+	//key nvalid URL escape "%"
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri21, res)
+	assert.NotNil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri22, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri23, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri24, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri25, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri26, res)
+	assert.Nil(t, err)
+
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri27, res)
+	assert.Nil(t, err)
+
+	//key nvalid URL escape "%"
+	res = KArr.NewStrMapItf()
+	err = KStr.ParseStr(tesUri28, res)
+	assert.NotNil(t, err)
+}
+
+func BenchmarkString_ParseStr(b *testing.B) {
+	b.ResetTimer()
+	res := KArr.NewStrMapItf()
+	for i := 0; i < b.N; i++ {
+		_ = KStr.ParseStr(tesUri1, res)
+	}
+}
+
+func TestString_ParseUrl(t *testing.T) {
+	var res map[string]string
+	var err error
+	var chk bool
+
+	res, err = KStr.ParseUrl(tesUrl01, -1)
+	assert.Nil(t, err)
+
+	res, err = KStr.ParseUrl(strHello, -1)
+	assert.Nil(t, err)
+
+	//错误的URL
+	res, err = KStr.ParseUrl(tesUrl02, -1)
+	assert.NotNil(t, err)
+	assert.Empty(t, res)
+
+	res, err = KStr.ParseUrl(tesUrl01, 1)
+	_, chk = res["scheme"]
+	assert.True(t, chk)
+
+	res, err = KStr.ParseUrl(tesUrl01, 2)
+	_, chk = res["host"]
+	assert.True(t, chk)
+
+	res, err = KStr.ParseUrl(tesUrl01, 4)
+	_, chk = res["port"]
+	assert.True(t, chk)
+
+	res, err = KStr.ParseUrl(tesUrl01, 8)
+	_, chk = res["user"]
+	assert.True(t, chk)
+
+	res, err = KStr.ParseUrl(tesUrl01, 16)
+	_, chk = res["pass"]
+	assert.True(t, chk)
+
+	res, err = KStr.ParseUrl(tesUrl01, 32)
+	_, chk = res["path"]
+	assert.True(t, chk)
+
+	res, err = KStr.ParseUrl(tesUrl01, 64)
+	_, chk = res["query"]
+	assert.True(t, chk)
+
+	res, err = KStr.ParseUrl(tesUrl01, 128)
+	_, chk = res["fragment"]
+	assert.True(t, chk)
+}
+
+func BenchmarkString_ParseUrl(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = KStr.ParseUrl(tesUrl01, -1)
+	}
+}
+
+func TestString_UrlEncodeUrlDecode(t *testing.T) {
+	var res1, res2 string
+	var err error
+
+	res1 = KStr.UrlEncode(tesStr1)
+	res2, err = KStr.UrlDecode(res1)
+	assert.Equal(t, res2, tesStr1)
+	assert.Nil(t, err)
+}
+
+func BenchmarkString_UrlEncode(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		KStr.UrlEncode(tesStr1)
+	}
+}
+
+func BenchmarkString_UrlDecode(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = KStr.UrlDecode(tesStr2)
+	}
+}
+
+func TestString_RawUrlEncodeRawUrlDecode(t *testing.T) {
+	var res1, res2 string
+	var err error
+
+	res1 = KStr.RawUrlEncode(tesStr3)
+	res2, err = KStr.RawUrlDecode(res1)
+	assert.Equal(t, res2, tesStr3)
+	assert.Nil(t, err)
+}
+
+func BenchmarkString_RawUrlEncode(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		KStr.RawUrlEncode(tesStr3)
+	}
+}
+
+func BenchmarkString_RawUrlDecode(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = KStr.RawUrlDecode(tesStr4)
+	}
+}
+
+func TestString_HttpBuildQuery(t *testing.T) {
+	var res string
+	params := url.Values{}
+	params.Add("a", "abc")
+	params.Add("b", "123")
+	params.Add("c", "你好")
+
+	res = KStr.HttpBuildQuery(params)
+	assert.Contains(t, res, "&")
+}
+
+func BenchmarkString_HttpBuildQuery(b *testing.B) {
+	b.ResetTimer()
+	params := url.Values{}
+	params.Add("a", "abc")
+	params.Add("b", "123")
+	params.Add("c", "你好")
+	for i := 0; i < b.N; i++ {
+		KStr.HttpBuildQuery(params)
+	}
+}
+
+func TestString_FormatUrl(t *testing.T) {
+	var res string
+
+	res = KStr.FormatUrl("")
+	assert.Empty(t, res)
+
+	res = KStr.FormatUrl(tesUrl03)
+	assert.Contains(t, res, "://")
+
+	res = KStr.FormatUrl(tesUrl04)
+	assert.Contains(t, res, "://")
+
+	res = KStr.FormatUrl(tesUrl05)
+	assert.NotContains(t, res, '\\')
+}
+
+func BenchmarkString_FormatUrl(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		KStr.FormatUrl(tesUrl05)
+	}
+}
+
+func TestString_GetDomain(t *testing.T) {
+	var tests = []struct {
+		param    string
+		isMain   bool
+		expected string
+	}{
+		{"", false, ""},
+		{strHello, false, ""},
+		{strSpeedLight, false, ""},
+		{tesUrl05, false, "login.localhost"},
+		{tesUrl06, false, "play.golang.com"},
+		{tesUrl07, true, "github.io"},
+		{tesUrl08, false, "foobar.中文网"},
+		{tesUrl09, false, "foobar.com"},
+		{localIp, false, "127.0.0.1"},
+	}
+	for _, test := range tests {
+		actual := KStr.GetDomain(test.param, test.isMain)
+		assert.Equal(t, actual, test.expected)
+	}
+}
+
+func BenchmarkString_GetDomain(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		KStr.GetDomain(tesUrl10)
+	}
+}
+
+func TestString_ClearUrlPrefix(t *testing.T) {
+	var tests = []struct {
+		url      string
+		prefix   string
+		expected string
+	}{
+		{"", "", ""},
+		{tesUrl10, "https://", "github.com/kakuilan/kgo"},
+		{tesUrl11, "/", "google.com/test?name=hello"},
+	}
+	for _, test := range tests {
+		actual := KStr.ClearUrlPrefix(test.url, test.prefix)
+		assert.Equal(t, actual, test.expected)
+	}
+}
+
+func BenchmarkString_ClearUrlPrefix(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		KStr.ClearUrlPrefix(tesUrl10)
+	}
+}
+
+func TestString_ClearUrlSuffix(t *testing.T) {
+	var tests = []struct {
+		url      string
+		prefix   string
+		expected string
+	}{
+		{"", "", ""},
+		{tesUrl10, "/kgo", "https://github.com/kakuilan"},
+		{tesUrl12, "/", "google.com/test?name=hello"},
+	}
+	for _, test := range tests {
+		actual := KStr.ClearUrlSuffix(test.url, test.prefix)
+		assert.Equal(t, actual, test.expected)
+	}
+}
+
+func BenchmarkString_ClearUrlSuffix(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		KStr.ClearUrlSuffix(tesUrl12)
 	}
 }
