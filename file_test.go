c69e94851240838c62d7d76c3b40693352564c4b
kakuilan <kakuilan@163.com>
2022-07-12 18:27:39 +0800

    fix TestFile_FastCopy

--- file_test.go
+++ file_test.go
@@ -590,13 +590,13 @@ func TestFile_FastCopy(t *testing.T) {
 
 	//源文件不存在
 	res, err = KFile.FastCopy(fileNone, fastcopyfile)
-	if KOS.IsLinux() {
-		assert.NotNil(t, err)
-	}
+	assert.NotNil(t, err)
 
 	//目录目录无权限
 	res, err = KFile.FastCopy(imgJpg, rootFile1)
-	assert.NotNil(t, err)
+	if KOS.IsLinux() {
+		assert.NotNil(t, err)
+	}
 
 	//目标为空
 	res, err = KFile.FastCopy(imgJpg, "")
