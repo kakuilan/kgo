6a7739734a87385b987c968648dd95badad64b40
kakuilan <kakuilan@163.com>
2022-07-28 14:27:13 +0800

    fix os_windows.HomeDir

--- os_windows.go
+++ os_windows.go
@@ -4,7 +4,6 @@
 package kgo
 
 import (
-	"errors"
 	"fmt"
 	"github.com/StackExchange/wmi"
 	"golang.org/x/sys/windows"
@@ -131,21 +130,9 @@ func getProcessPathByPid(pid int) (res string) {
 
 // HomeDir 获取当前用户的主目录.
 func (ko *LkkOS) HomeDir() (string, error) {
-	// First prefer the HOME environmental variable
-	if home := os.Getenv("HOME"); home != "" {
-		return home, nil
-	}
-
-	// Prefer standard environment variable USERPROFILE
-	if home := os.Getenv("USERPROFILE"); home != "" {
-		return home, nil
-	}
-
-	drive := os.Getenv("HOMEDRIVE")
-	path := os.Getenv("HOMEPATH")
-	home := drive + path
-	if drive == "" || path == "" {
-		return "", errors.New("[HomeDir] HOMEDRIVE, HOMEPATH, or USERPROFILE are blank")
+	home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+	if home == "" {
+		home = os.Getenv("USERPROFILE")
 	}
 
 	return home, nil
