bfaa31f3f6facf14d4b288f48f639d976d9941a3
kakuilan <kakuilan@163.com>
2022-11-29 11:45:36 +0800

    fix GetBoardInfo,replace KStr.Trim to trim

--- os_darwin.go
+++ os_darwin.go
@@ -135,7 +135,7 @@ func (ko *LkkOS) GetBiosInfo() *BiosInfo {
 	infos := ko.getIOInfos()
 	if len(infos) > 0 {
 		infoStr := string(infos)
-		res.Version = KStr.Trim(KStr.GetEquationValue(infoStr, "SMBIOS-EPS"), "<", ">", `"`, `'`)
+		res.Version = trim(KStr.GetEquationValue(infoStr, "SMBIOS-EPS"), "<", ">", `"`, `'`)
 	}
 
 	return res
@@ -154,9 +154,9 @@ func (ko *LkkOS) GetBoardInfo() *BoardInfo {
 	infos := ko.getIOInfos()
 	if len(infos) > 0 {
 		infoStr := string(infos)
-		res.Name = KStr.Trim(KStr.GetEquationValue(infoStr, "product-name"), "<", ">", `"`, `'`)
-		res.Version = KStr.Trim(KStr.GetEquationValue(infoStr, "board-id"), "<", ">", `"`, `'`)
-		res.Serial = KStr.Trim(KStr.GetEquationValue(infoStr, "IOPlatformUUID"), "<", ">", `"`, `'`)
+		res.Name = trim(KStr.GetEquationValue(infoStr, "product-name"), "<", ">", `"`, `'`)
+		res.Version = trim(KStr.GetEquationValue(infoStr, "board-id"), "<", ">", `"`, `'`)
+		res.Serial = trim(KStr.GetEquationValue(infoStr, "IOPlatformUUID"), "<", ">", `"`, `'`)
 	}
 
 	return res
